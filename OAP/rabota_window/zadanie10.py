2e51f2603e637121967ec97b1948a66c84643223
IDLY <oleg433242new@gmail.com>
2025-05-05 12:02:13 +0300

    Update zadanie10.py

--- OAP/rabota_window/zadanie10.py
+++ OAP/rabota_window/zadanie10.py
@@ -1,1 +1,27 @@
+from tkinter import *
+import tkinter.messagebox as box
 
+window = Tk()
+window.title("Ввод данных")
+
+frame = Frame(window)  # Создаем фрейм, в который будет размещено поле для ввода
+entry = Entry(frame)  # Создаем поле ввода и привязываем его к фрейму
+
+# Функция для отображения данных, считанных из поля ввода
+def dialog():
+    box.showinfo("Приветствие", "Привет, " + entry.get())  # Исправлено на entry
+
+# Создаем кнопку, которая будет вызывать функцию dialog()
+btn = Button(frame, text="Ввод", command=dialog)
+
+# Создаем метку с поясняющим текстом
+lb = Label(frame, text="Введите имя: ")
+
+# Добавляем все виджеты на фрейм
+lb.pack(side=LEFT)
+entry.pack(side=LEFT)
+btn.pack(side=RIGHT, padx=5)
+
+frame.pack(padx=20, pady=20)
+
+window.mainloop()  # Цикл обработки событий окна
