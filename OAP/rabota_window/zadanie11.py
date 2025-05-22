e7024ac320d937fd55793a5ae9da7f367189d6b8
IDLY <oleg433242new@gmail.com>
2025-05-22 11:20:58 +0300

    Update zadanie11.py

--- OAP/rabota_window/zadanie11.py
+++ OAP/rabota_window/zadanie11.py
@@ -1,1 +1,12 @@
+from tkinter import *
 
+# Создаем окно
+window = Tk()
+window.title("Многострочный текст")  # Заголовок окна
+
+# Создаем многострочное поле ввода
+text = Text(width=30, height=10, bg="blue", fg="yellow", wrap=WORD)
+text.pack()
+
+# Цикл обработки событий окна
+window.mainloop()
